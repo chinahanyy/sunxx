418a9f5fea0942aef90f1194a71a90b3f88f8740
chinahanyy <794942862@qq.com>
2019-08-28 17:02:29 +0800

    11

---
+++ 03.js
@@ -0,0 +1,24 @@
+// git =》github     码云（Git svn）
+// 分布式管理系统
+// 三端：本地区， 暂存区， 远端区
+
+// svn
+// 两端：本地 远端
+// conmmit 提交 本地 =》 远端   
+// update   更新  （拉取）  远端=》本地
+
+// git 可视化 sourcetree
+
+
+// $ cd abc
+// git init 项目初始化
+// git status 查看文件状态
+// git add<file> 添加单个文件
+// git add<file>  <file>添加多个
+// git add.      添加所有
+// git commit -m 提交到暂存区 -M"注释 必须写"
+// git config --global user.name "用户名"//第一次用
+// git config-- global user.email "邮箱"//第一次用
+// git remote add origin 地址  链接远程仓库
+// git push -u origin master 第一次提交
+// git push
